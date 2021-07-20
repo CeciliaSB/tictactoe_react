c0753517ff350d2eb821979b3c4f068e557e4bfb
CeciliaSB <cstefany22@gmail.com>
2021-07-20 11:17:44 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Board from './components/Board';
+import Modal from './components/Modal';
+import { calculateWinner } from './helper';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const [showHide, setShowHide] = useState(true);
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState(true);
+  const [playerName, setPlayerName] = useState({player1: "", player2: ""});
+  
+
+  function assingPlayer1(e) {
+    setPlayerName({
+      ...playerName,
+      player1: e.target.value,
+    });
+  }
+
+  function assingPlayer2(e) {
+    setPlayerName({
+      ...playerName,
+      player2: e.target.value,
+    });
+  }
+
+  
+ function hideModal() {
+   if (showHide) {
+     setSquares(Array(9).fill(null));
+   }
+   setShowHide(!showHide);
+ } 
+
+ function hideModal1() {
+   setShowHide(!showHide);
+   setPlayer(true);
+ }
+
+ function hideModal2() {
+   setShowHide(!showHide);
+   setPlayer(false);
+ }
+ 
+  function handleClick(i) {
+    let newSquares =[...squares];
+    if (calculateWinner(squares) || newSquares[i]) return;
+    newSquares[i] = player ? "X" : "O";
+    setSquares(newSquares);
+    setPlayer(!player);
+  }
+
+  const checkForWinner = calculateWinner(squares);
+  const draw = squares.includes(null);
+
+  let playerTurn;
+  playerTurn = draw
+    ? checkForWinner
+      ? player
+        ? `Winner: ${playerName.player2} (${checkForWinner})`
+        : `Winner: ${playerName.player1} (${checkForWinner}`
+      :`Next player: ${
+          player ? playerName.player1 + " (X) " : playerName.player2 + " (O) "
+        }`
+    : "Tie";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Tic Tac Toe</h1>
+      <Modal showHide={showHide} playerName1={playerName.player1} playerName2={playerName.player2} assingP1={assingPlayer1} assingP2={assingPlayer2} hideModal1={hideModal1} hideModal2={hideModal2}/>
+      <Board showHide={showHide} hideModal={hideModal} playerTurn={playerTurn} squares={squares} handleClick={handleClick} />
     </div>
   );
 }
